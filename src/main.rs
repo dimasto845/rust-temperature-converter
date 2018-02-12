913393460be4a1cfb93895c6209206a60e671805
jon.bradford@bathasu.com <548416+jbradford@users.noreply.github.com>
2018-02-12 10:57:22 +0000

    Finished initial implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+const SUBTRACT_NUM: f64 = 32.0;
+const WEIGHT_FACTOR: f64 = 5.0/9.0;
+
 fn main() {
-    println!("Hello, world!");
+    let boiling = celcius_to_fahrenheit(100.0);
+    let freezing = fahrenheit_to_celcius(32.0);
+    println!("Boiling in fahrenheit is {}.", boiling);
+    println!("Freezing in celcius is {}.", freezing);
+}
+
+fn celcius_to_fahrenheit(temp:f64)->f64{
+    temp / WEIGHT_FACTOR + SUBTRACT_NUM
+}
+
+fn fahrenheit_to_celcius(temp:f64)->f64{
+    (temp - SUBTRACT_NUM) * WEIGHT_FACTOR
 }
